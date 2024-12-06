dc2863b86ad6931a5b799d3c6005697ab5db785e
jkmancuso <93846640+jkmancuso@users.noreply.github.com>
2024-12-06 18:32:00 -0500

    add some mocking

--- shared/dynamo_mock.go
+++ shared/dynamo_mock.go
@@ -2,7 +2,8 @@ package shared
 
 import (
 	"context"
-
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
@@ -14,6 +15,7 @@ type DynamoClientInterface interface {
 }
 
 type DynamoClientMock struct {
+	mockedRow map[string]types.AttributeValue
 }
 
 func (client *DynamoClientMock) Scan(ctx context.Context, input *dynamodb.ScanInput, opts ...func(*dynamodb.Options)) (*dynamodb.ScanOutput, error) {
@@ -25,7 +27,24 @@ func (client *DynamoClientMock) PutItem(ctx context.Context, input *dynamodb.Put
 }
 
 func (client *DynamoClientMock) GetItem(ctx context.Context, input *dynamodb.GetItemInput, opts ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error) {
-	return &dynamodb.GetItemOutput{}, nil
+	
+	//loop through the mock row 
+	for mockKey, mockVal := range client.mockedRow {
+		// if the key exists (ie input["id"]) in both the mock and the input 
+		if  _, ok := input[mockKey]; ok {
+			// AND the value is found
+			if mockVal == input[mockKey]{
+				//then return the mock row
+				return &dynamodb.GetItemOutput{
+					Item: client.mockedRow
+				}, nil
+			}
+		}
+	} 
+	//else return empty row
+	return &dynamodb.GetItemOutput{
+		Item: map[string]types.AttributeValue{}
+	}, nil
 }
 
 func (client *DynamoClientMock) DeleteItem(ctx context.Context, input *dynamodb.DeleteItemInput, opts ...func(*dynamodb.Options)) (*dynamodb.DeleteItemOutput, error) {
